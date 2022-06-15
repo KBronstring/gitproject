4752fcc2c485141f3e21a549acba1312f6ea78bf
Koen Bronstring <kbronstring@gmail.com>
2022-06-15 17:25:04 +0200

    added tests

--- kbronstring/tests/test_app.py
+++ kbronstring/tests/test_app.py
@@ -17,7 +17,22 @@ def test_squareroot(client):
     answer = client.get("squareroot_number?number=4")
     assert b"Square Root of 4.0 is 2.0" == answer.data
 
+def test_square(client):
+    answer = client.get("square_number?number=2")
+    assert b"Square of 2.0 is 4.0" == answer.data
 
 def test_hello_world(client):
     answer = client.get("/")
     assert re.match(b".* on host .*", answer.data)
+
+def test_empty_input(client):
+    answer = client.get("")
+    assert 'Empty Input'== answer.data
+
+def not_numeric(client):
+    answer = client.get("square_number?number=a")
+    assert b"Not a numerical value" == answer.data
+
+def test_other_square(client):
+    answer = client.get("square_number?number=3")
+    assert b"Square of 3.0 is 9.0" == answer.data
